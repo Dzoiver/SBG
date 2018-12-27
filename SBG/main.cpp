3744e3609e572356d925985296d949cf9ad0da33
Кирилл Безруков <doomseifer@mail.ru>
2018-12-27 10:42:44 +0400

    Animated sprite

--- SBG/main.cpp
+++ SBG/main.cpp
@@ -25,11 +25,19 @@ SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
 
 SDL_Rect gSpriteClips[4];
+
+const int SPAWN_ANIMATION_FRAMES = 10;
+SDL_Rect gSpawnClips[10];
 LTexture gSpriteSheetTexture;
+LTexture gSpawnSheetTexture;
 LTexture gModulatedTexture;
 
 LTexture gFooTexture;
 LTexture gBackgroundTexture;
+
+enum Walking {
+
+};
 
 LTexture::LTexture()
 {
@@ -167,7 +175,7 @@ bool init()
 		else
 		{
 			//Create renderer for window
-			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 			if (gRenderer == NULL)
 			{
 				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
@@ -197,7 +205,7 @@ bool loadMedia()
 	//Loading success flag
 	bool success = true;
 
-	if (!gSpriteSheetTexture.loadFromFile("res/player_sheet2.png"))
+	if (!gSpriteSheetTexture.loadFromFile("res/player_sheet4.png"))
 	{
 		printf("Failed to load Soldier texture!\n");
 		success = false;
@@ -224,6 +232,111 @@ bool loadMedia()
 		gSpriteClips[3].y = 0;
 		gSpriteClips[3].w = 99;
 		gSpriteClips[3].h = 98;
+		////////////////////////////////////////////////
+		/*
+		//left
+		gSpriteClips[7].x = 297;
+		gSpriteClips[7].y = 98;
+		gSpriteClips[7].w = 99;
+		gSpriteClips[7].h = 98;
+
+		gSpriteClips[11].x = 297;
+		gSpriteClips[11].y = 196;
+		gSpriteClips[11].w = 99;
+		gSpriteClips[11].h = 98;
+		
+		//right
+		gSpriteClips[5].x = 99;
+		gSpriteClips[5].y = 98;
+		gSpriteClips[5].w = 99;
+		gSpriteClips[5].h = 98;
+
+		gSpriteClips[9].x = 99;
+		gSpriteClips[9].y = 196;
+		gSpriteClips[9].w = 99;
+		gSpriteClips[9].h = 98;
+		
+		//////////////////////////////////////////////////
+		//up
+		gSpriteClips[4].x = 0;
+		gSpriteClips[4].y = 98;
+		gSpriteClips[4].w = 99;
+		gSpriteClips[4].h = 98;
+
+		gSpriteClips[8].x = 0;
+		gSpriteClips[8].y = 196;
+		gSpriteClips[8].w = 99;
+		gSpriteClips[8].h = 98;
+		
+		//down
+		gSpriteClips[6].x = 196;
+		gSpriteClips[6].y = 98;
+		gSpriteClips[6].w = 99;
+		gSpriteClips[6].h = 98;
+
+		gSpriteClips[10].x = 198;
+		gSpriteClips[10].y = 196;
+		gSpriteClips[10].w = 99;
+		gSpriteClips[10].h = 98;*/
+	}
+	if (!gSpawnSheetTexture.loadFromFile("res/spawn.png"))
+	{
+		printf("Failed to load spawn.png\n");
+		success = false;
+	}
+	else
+	{
+		// 42 56
+		//offset 21 28
+		gSpawnClips[0].x = 0;
+		gSpawnClips[0].y = 0;
+		gSpawnClips[0].w = 42;
+		gSpawnClips[0].h = 56;
+
+		gSpawnClips[1].x = 42;
+		gSpawnClips[1].y = 0;
+		gSpawnClips[1].w = 42;
+		gSpawnClips[1].h = 45;
+
+		gSpawnClips[2].x = 86;
+		gSpawnClips[2].y = 0;
+		gSpawnClips[2].w = 40;
+		gSpawnClips[2].h = 37;
+
+		gSpawnClips[3].x = 127;
+		gSpawnClips[3].y = 0;
+		gSpawnClips[3].w = 30;
+		gSpawnClips[3].h = 34;
+		//offset 21 28
+		gSpawnClips[4].x = 160;
+		gSpawnClips[4].y = 0;
+		gSpawnClips[4].w = 17;
+		gSpawnClips[4].h = 16;
+
+		gSpawnClips[5].x = 181;
+		gSpawnClips[5].y = 0;
+		gSpawnClips[5].w = 9;
+		gSpawnClips[5].h = 8;
+
+		gSpawnClips[6].x = 195;
+		gSpawnClips[6].y = 0;
+		gSpawnClips[6].w = 3;
+		gSpawnClips[6].h = 3;
+
+		gSpawnClips[7].x = 195;
+		gSpawnClips[7].y = 49;
+		gSpawnClips[7].w = 7;
+		gSpawnClips[7].h = 7;
+
+		gSpawnClips[8].x = 181;
+		gSpawnClips[8].y = 9;
+		gSpawnClips[8].w = 13;
+		gSpawnClips[8].h = 13;
+		//offset 21 28
+		gSpawnClips[9].x = 195;
+		gSpawnClips[9].y = 9;
+		gSpawnClips[9].w = 17;
+		gSpawnClips[9].h = 17;
 	}
 	if (!gModulatedTexture.loadFromFile("res/colors.png"))
 	{
@@ -247,6 +360,7 @@ void close()
 {
 	//Free loaded image
 	gBackgroundTexture.free();
+	gSpriteSheetTexture.free();
 
 	//Destroy window
 	SDL_DestroyRenderer(gRenderer);
@@ -278,8 +392,11 @@ int main(int argc, char* args[])
 			//Main loop flag
 			bool quit = false;
 
+
 			//Event handler
 			SDL_Event e;
+
+			int frame = 0;
 
 			Uint8 r = 255;
 			Uint8 g = 255;
@@ -288,8 +405,9 @@ int main(int argc, char* args[])
 			Uint8 a = 255;
 
 			int i = 0;
-
+			bool walking = false;
 			//While application is running
+			printf("initialization completed");
 			while (!quit)
 			{
 				//Handle events on queue
@@ -372,12 +490,18 @@ int main(int argc, char* args[])
 							break;
 						}
 					}
-
 				}
+
 
 				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 				SDL_RenderClear(gRenderer);
-
+				SDL_Rect* currentClip = &gSpawnClips[frame / 20];
+				gSpawnSheetTexture.render(800, 500, currentClip);
+				++frame;
+				if (frame / 20 >= SPAWN_ANIMATION_FRAMES)
+				{
+					frame = 0;
+				}
 
 				gModulatedTexture.setColor(r, g, b);
 				//Render background texture to screen
